e83ad7ac2ddaeabe3c14be5f80d470cf21493b1f
Veritan <7586691+veritangamingTTV@users.noreply.github.com>
2021-07-07 21:36:38 -0400

    Update Settings.py
    Added clarification for each variable

--- Settings.py
+++ Settings.py
@@ -3,6 +3,15 @@
 # Necessary settings are
 # HOST, PORT, PASS, NICK, CHANNEL
 #
+# HOST and PORT are static,
+# always "irc.twitch.tv" and 6667
+#
+# PASS is Bot Account's private OAUTH token
+#
+# NICK is bot's twitch username
+#
+# CHANNEL is the stream channel in all lowercase
+#
 # Additional settings may be added as the project grows
 
 HOST = "irc.twitch.tv"
